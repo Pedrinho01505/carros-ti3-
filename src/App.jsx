c96b555503e4cad4bac79b0552492ca0f55321f3
Pedrinho01505 <pedrinhoavare18@gmail.com>
2024-03-28 13:27:10 -0300

    ajuste final

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Luxury from "./components/Luxury"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <div className="lg:flex max-w[920px]">
+      <div className="lg:flex max-w-5xl">
         <Sedans/>   
         <Suvs/>
         <Luxury/>     
